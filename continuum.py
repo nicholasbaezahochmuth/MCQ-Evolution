fac347e9cdc50947333ee7707b82fdec72515f23
Nicholas Baeza Hochmuth <nicholas.baezahochmuth@gmail.com>
2016-07-27 11:16:35 -0400

    Update continuum.py

--- continuum.py
+++ continuum.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+## simple continuum() function to concatenate the two flat sections of the spectra on either side of the 
+## H-delta absorption line
+
 def continuum(lam, flux):    
     index1 = np.where((lam > 4046) & (lam < 4076))
     index2 = np.where((lam > 4130) & (lam < 4150))
