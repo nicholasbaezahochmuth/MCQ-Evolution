58f2aa2a69baa3373d9cce0b5952e938f1ceeb39
Nicholas Baeza Hochmuth <nicholas.baezahochmuth@gmail.com>
2016-07-27 10:56:30 -0400

    Update read_sdss.py

--- read_sdss.py
+++ read_sdss.py
@@ -1,6 +1,8 @@
 
 #http://stackoverflow.com/questions/19206332/gaussian-fit-for-python
 
+##     Import of all of the packages used in this project, for reference at least
+
 import pylab as plb
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -12,9 +14,18 @@ from matplotlib.pyplot import *
 import numpy
 from scipy import interpolate
 
+##       using SDSS MCQ catalogue FITS file to create "cat" data table
+##       isolate the cat_file (file name) and cat_z (redshift) data to use
+##       in the read_sdss() function
+
 cat = fits.getdata('//Python/sdss_compact_dr12_sn8_oii.fits', 1)
 cat_file = cat['file']
 cat_z = cat['z']
+
+##      read_sdss() function to read in galaxy spectra file and return corrected wavelength and standard deviation
+##          input: filename, redshift
+##          output: image_data.lam (wavelength corrected for redshift and originally log format)
+##                  image_data.sigma (inverse variance converted to standard deviation)
 
 def read_sdss(name, z=0.):
     
